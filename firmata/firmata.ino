9ea14bae45e713191fddd9bc285e11c1139b3aa5
Jenny-NPM <43864193+Jenny-NPM@users.noreply.github.com>
2018-10-04 14:03:49 -0700

    Add files via upload
    code for 1 arduino input (lickometer, ect)

---
+++ firmata/firmata.ino
@@ -0,0 +1,28 @@
+#include <Firmata.h>
+
+byte analogPin;
+
+void analogWriteCallback(byte pin, int value)
+{
+  pinMode(pin, OUTPUT);
+  analogWrite(pin, value);
+}
+
+void setup()
+{
+  Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
+  Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
+  Firmata.begin();
+}
+
+void loop()
+{
+  while (Firmata.available()) {
+    Firmata.processInput();
+  }
+ 
+    Firmata.sendAnalog(0, (analogRead(0)*(5000/1024)));
+    delay(1);
+   
+  }
+
